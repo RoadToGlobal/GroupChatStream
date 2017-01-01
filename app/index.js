c39a49167d2c0b45e78515a77059d4c18a7453ef
rickisen <rickard.spam@gmail.com>
2017-01-01 21:41:11 +0100

    prepair for deployment

--- app/index.js
+++ app/index.js
@@ -109,6 +109,6 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(8000, () => {
-  console.log('listening on port 8000');
+server.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`);
 });
